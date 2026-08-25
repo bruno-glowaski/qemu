f4a1f0d28c97e26e18f45f922687f28b2be1a7c3
Bruno Henrique Glowaski Morais <bruno.glowaski@outlook.com>
2026-08-25 11:30:34 -0400

    fix: use 42 as virtio-pc id to fix probe

--- include/standard-headers/linux/virtio_ids.h
+++ include/standard-headers/linux/virtio_ids.h
@@ -69,7 +69,8 @@
 #define VIRTIO_ID_BT 40             /* virtio bluetooth */
 #define VIRTIO_ID_GPIO 41           /* virtio gpio */
 #define VIRTIO_ID_SPI 45            /* virtio spi */
-#define VIRTIO_ID_PC 74
+
+#define VIRTIO_ID_PC 42
 
 /*
  * Virtio Transitional IDs
--- linux-virtio-pc-module/virtio-pc.h
+++ linux-virtio-pc-module/virtio-pc.h
@@ -5,7 +5,7 @@ enum virtio_pc_cmd {
   VIRTIO_PC_CMD_START = 0,
 };
 
-#define VIRTIO_ID_PC 74
+#define VIRTIO_ID_PC 42
 #define WORK_QUEUE_NAME "work_queue"
 
 #endif
